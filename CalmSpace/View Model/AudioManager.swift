cbddee45636a8a7be30340470729bb3ed8e04619
mieng18 <mieng18@hotmail.com>
2023-07-09 15:37:08 -0700

    Create stop and play method

---
+++ CalmSpace/View Model/AudioManager.swift
@@ -0,0 +1,147 @@
+//
+//  AudioManager.swift
+//  CalmSpace
+//
+//  Created by mai nguyen on 7/9/23.
+//
+
+import SwiftUI
+import AVKit
+import Foundation
+
+
+
+final class AudioManager: ObservableObject {
+    //static let shared = AudioManager()
+    var player: AVAudioPlayer?
+    @Published private(set) var isPlaying: Bool = false {
+        didSet {
+            print("isPlaying", isPlaying)
+        }
+    }
+
+    @Published private(set) var isLooping: Bool = false {
+        didSet {
+            print("isPlaying", isLooping)
+        }
+    }
+    
+    func startPlayer(track: String, isPreview: Bool = false) {
+        guard let url = Bundle.main.url(forResource: track, withExtension: "mp3") else {
+            print("Resource not found \(track)")
+            return
+        }
+
+        do{
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url)
+
+            if isPreview {
+                player?.prepareToPlay()
+            } else {
+                player?.play()
+                isPlaying = true
+            }
+
+        } catch {
+            print("Fail to initialize the player", error)
+        }
+    }
+
+    func playPause() {
+        guard let player = player else {
+            print("Instance of audio player not found")
+            return
+        }
+        if player.isPlaying {
+            player.pause()
+            isPlaying = false
+        } else {
+            player.play()
+            isPlaying = true
+        }
+    }
+
+    
+    func stop() {
+        guard let player = player else { return }
+        if player.isPlaying {
+            player.stop()
+        }
+    }
+    
+    func toggleLoop() {
+        guard let player = player else { return }
+        player.numberOfLoops = player.numberOfLoops == 0 ? -1 : 0
+        isLooping = player.numberOfLoops != 0
+        print("isLooping",isLooping)
+    }
+    
+    
+}
+//final class AudioManager: ObservableObject {
+//    //static let shared = AudioManager()
+//    var player: AVAudioPlayer?
+//    @Published private(set) var isPlaying: Bool = false {
+//        didSet {
+//            print("isPlaying", isPlaying)
+//        }
+//    }
+//
+//    @Published private(set) var isLooping: Bool = false {
+//        didSet {
+//            print("isPlaying", isLooping)
+//        }
+//    }
+//    func startPlayer(track: String, isPreview: Bool = false) {
+//        guard let url = Bundle.main.url(forResource: track, withExtension: "mp3") else {
+//            print("Resource not found \(track)")
+//            return
+//        }
+//
+//        do{
+//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//            player = try AVAudioPlayer(contentsOf: url)
+//
+//            if isPreview {
+//                player?.prepareToPlay()
+//            } else {
+//                player?.play()
+//                isPlaying = true
+//            }
+//
+//        } catch {
+//            print("Fail to initialize the player", error)
+//        }
+//    }
+
+//    func playPause() {
+//        guard let player = player else {
+//            print("Indtsnce of audio player not found")
+//            return
+//        }
+//        if player.isPlaying {
+//            player.pause()
+//            isPlaying = false
+//        } else {
+//            player.play()
+//            isPlaying = true
+//        }
+//    }
+
+//    func stop() {
+//        guard let player = player else { return }
+//        if player.isPlaying {
+//            player.stop()
+//        }
+//    }
+//
+//    func toggleLoop() {
+//        guard let player = player else { return }
+//        player.numberOfLoops = player.numberOfLoops == 0 ? -1 : 0
+//        isLooping = player.numberOfLoops != 0
+//        print("isLooping",isLooping)
+//    }
+//}
--- CalmSpace/View/PlayerView.swift
+++ CalmSpace/View/PlayerView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct PlayerView: View {
     @EnvironmentObject var audioManager: AudioManager
     var meditationVM: MeditationViewModel
+    
     var isPreview: Bool = false
+    @State private var isEditing: Bool = false
     @State private var value: Double = 0.0
     @Environment(\.dismiss) var dismiss
     
@@ -18,108 +20,117 @@ struct PlayerView: View {
     let timer = Timer
         .publish(every: 0.5, on: .main, in: .common)
         .autoconnect()
-    
+
     var body: some View {
-        
         ZStack {
+            // MARK: Background Image
             Image(meditationVM.meditation.image)
                 .resizable()
                 .scaledToFill()
                 .frame(width: UIScreen.main.bounds.width)
                 .ignoresSafeArea()
             
-            
-            // MARK: Blur View
-            
+            // MARK: Blur view
             Rectangle()
-                .background(.thinMaterial)
+                .background(.ultraThinMaterial)
                 .opacity(0.25)
                 .ignoresSafeArea()
             
-            VStack(spacing:32) {
-                
+            
+            // MARK: Dismiss Button
+            VStack(spacing: 32) {
                 HStack{
-                    Button{
+                    Spacer()
+                    
+                    Button {
+                        audioManager.stop()
                         dismiss()
-                        
-                    }label: {
+                    } label: {
                         Image(systemName: "xmark.circle.fill")
-                            .font(.system(size:36))
-                            .foregroundColor(.white)
+                            .font(.system(size: 36))
                         
                     }
-                    
-                    Spacer()
                 }
+                .padding(20)
+                .foregroundColor(.white)
                 
                 // MARK: Title
-                
                 Text(meditationVM.meditation.title)
                     .font(.title)
+                    .foregroundColor(.white)
                 
                 Spacer()
-                
-                VStack(spacing:5 ){
-                    // MARK: Playback Timeline
-                    
-                    Slider(value: $value, in: 0...60)
-                        .accentColor(.white)
-                    
-                    // MARK: Playback Timeline
-                    
-                    HStack {
-                        Text("0:00")
+                if let player = audioManager.player {
+                    VStack(spacing: 10) {
+                        // MARK: Playback timeline
+                        Slider(value: $value, in: 0...player.duration) { editing in
+                            print("editing", editing)
+                            isEditing = editing
+                            if !editing {
+                                player.currentTime = value
+                            }
+                        }
+                            .accentColor(.white)
                         
-                        Spacer()
+                        // MARK: Playback time
+                        HStack {
+                            Text(DateComponentsFormatter.positional.string(from: player.currentTime) ?? "0:00")
+                            Spacer()
+                            Text(DateComponentsFormatter.positional.string(from: player.duration - player.currentTime) ?? "0:00")
+                        }
+                        .foregroundColor(.white)
+                        .font(.caption)
                         
-                        Text("1:00")
+                        HStack{
+                            // MARK: Repeat Button
+                            let color: Color = audioManager.isLooping ? .orange : .white
+                            PlaybackControlButton(systemName: "repeat") {
+                                audioManager.toggleLoop()
+                            }
+                            
+                            Spacer()
+                            
+                            // MARK: Backward Button
+                            PlaybackControlButton(systemName: "gobackward.10") {
+                                player.currentTime -= 10
+                            }
+                            
+                            Spacer()
+                            
+                            // MARK: play Button
+                            
+                            PlaybackControlButton(systemName: audioManager.isPlaying ? "pause.circle.fill" : "play.circle.fill",fontSize: 44) {
+                                audioManager.playPause()
+                            }
+                            
+                            Spacer()
+                            
+                            // MARK: Forward Button
+                            PlaybackControlButton(systemName: "goforward.10") {
+                                player.currentTime += 10
+                            }
+                            
+                            Spacer()
+                            
+                            // MARK: Stop Button
+                            PlaybackControlButton(systemName: "stop.fill") {
+                                audioManager.stop()
+                                dismiss()
+                            }
+                        }
                     }
-                    .font(.caption)
-                    .foregroundColor(.white)
+                    .padding(20)
                 }
-                
-                HStack{
-                    PlaybackControlButton(systemName: "repeat") {
-                       
-                    }
-                    Spacer()
-                    
-                    PlaybackControlButton(systemName: "gobackward.10") {
-                       
-                    }
-                    
-                    Spacer()
-
-                    PlaybackControlButton(systemName: "play.circle.fill") {
-                       
-                    }
-                    
-                    Spacer()
-                    
-                    PlaybackControlButton(systemName: "goforward.10") {
-                       
-                    }
-                    
-                    Spacer()
-                    
-                    PlaybackControlButton(systemName: "stop.fill") {
-                       
-                    }
-                }
-                
             }
-            .padding(20)
+            
         }
         .onAppear{
-//            AudioManager.shared.startPlayer(track: meditationVM.meditation.track,isPreview: isPreview)
-            audioManager.startPlayer(track: meditationVM.meditation.track,isPreview: isPreview)
+            audioManager.startPlayer(track: meditationVM.meditation.track, isPreview: isPreview)
         }
         .onReceive(timer) { _ in
-            guard let player = audioManager.player else {return}
+            guard let player = audioManager.player, !isEditing else { return }
             value = player.currentTime
         }
-        
-
     }
 }
 
